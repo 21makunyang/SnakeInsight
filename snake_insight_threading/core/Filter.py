5a86e323402c8b0cbfe444cf414199857dc7e8c8
mky <1748438065@qq.com>
2024-04-19 00:22:01 +0800

    feat(Filter): Add get_room_price

--- snake_insight_threading/core/Filter.py
+++ snake_insight_threading/core/Filter.py
@@ -1,5 +1,5 @@
 import time
-
+import numpy as np
 from snake_insight_threading.util import RedisCommand
 
 
@@ -12,10 +12,9 @@ class Filter(object):
         self.get_city_info_by_area(area)
 
     def get_city_info_by_area(self, area):
-        if area is None:
+        if area is None or area == self.last_area:
             return self.city_infos
-        if area == self.last_area:
-            return self.city_infos
+
         self.city_infos = []
         house_info_keys = self.redis.smembers(area)
 
@@ -29,13 +28,38 @@ class Filter(object):
                 house_info_utf8 = {k.decode('utf8'): v.decode('utf8') for k, v in house_info.items()}
                 house_info_utf8['tags'] = str(house_info_utf8.get('tags')[1:-1]).split(',')
                 self.city_infos.append(house_info_utf8)
+
+        self.last_area = area
         return self.city_infos
+
+    def get_room_price(self, area):
+        self.get_city_info_by_area(area)
+
+        # key为living_room和bedroom构成的元组，value为列表，分别为平均价 最高价 最低价 统计数量
+        statisticians_result = {}
+
+        for house_info in self.city_infos:
+            living_room = house_info.get('living_room')
+            bedroom = house_info.get('bedroom')
+            price = house_info.get('price')
+
+            statisticians = statisticians_result.get((living_room, bedroom),[0, price, price, 0])
+            statisticians[0] += price
+            statisticians[1] = max(statisticians[1], price)
+            statisticians[2] = min(statisticians[2], price)
+            statisticians[3] += 1
+            statisticians_result[(living_room, bedroom)] = statisticians
+
+        for k in statisticians_result:
+            statisticians_result[k][0] = statisticians_result[k][0]/statisticians_result[k][3]
+
+        return statisticians_result
 
 
 if __name__ == "__main__":
     filter = Filter()
     start = time.time()
-    res = filter.get_city_info_by_area('海珠')
+    res = filter.get_room_price('海珠')
     end = time.time()
     print(res)
-    print('finished\ncost: {}'.format(str(end-start)))
+    print('finished\ncost: {}'.format(str(end - start)))
