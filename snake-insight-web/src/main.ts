14f26f77d84730263530144af7167e45af15261d
Canbohe54 <Canbohe54@outlook.com>
2024-04-18 20:34:43 +0800

    no more js import!

--- snake-insight-web/src/main.ts
+++ snake-insight-web/src/main.ts
@@ -7,8 +7,6 @@ import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import ECharts from 'vue-echarts'
 import 'echarts'
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
 import locale from 'element-plus/es/locale/lang/zh-cn'
 import $ from 'jquery'
 
@@ -18,7 +16,6 @@ pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus,{locale})
 app.component('ECharts', ECharts)
 app.config.globalProperties.$ = $
 
