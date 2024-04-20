59bde0fa49a72e5ea702d88208a7fe39007501bb
mky <1748438065@qq.com>
2024-04-20 16:50:50 +0800

    feat(Divider, Filter): Add function divide_by_region. Add some data preprocess in Filter
    还差小区类型（自建房、居民楼、写居一体楼、小区、高级小区、别墅）-每平方米价格
    各区域租房数据量、发布房源最多的商圈TOP10、出租最多的厅室类型
    用户选择区域，输入价格、层高、几室几厅，大约能租到多大的房子
    用户选择区域，输入房间大小、层高、几室几厅，大约需多少价格。

--- snake_insight_threading/core/Divider.py
+++ snake_insight_threading/core/Divider.py
@@ -4,7 +4,7 @@ from snake_insight_threading.util import RedisCommand
 
 
 class Divider(object):
-    redis = RedisCommand(host='10.242.91.125')
+    redis = RedisCommand(db=1)
     city_infos = []
     last_area = ''
 
@@ -19,11 +19,23 @@ class Divider(object):
             house_info = self.redis.hget(key)
             self.redis.sadd(house_info.get(b'area').decode('utf8'), key)
 
+    def divide_by_region(self):
+
+        house_info_keys = self.redis.scan(match=None, count=10000, _type='HASH')
+        index = 0
+        for key in house_info_keys:
+            # print(self.redis.hget(key))
+            index += 1
+            print(index)
+            house_info = self.redis.hget(key)
+            self.redis.sadd(house_info.get(b'region').decode('utf8'), key)
+
+
 
 if __name__ == "__main__":
     area = Divider()
     start = time.time()
-    print(area.divide_by_area())
+    print(area.divide_by_region())
     # time.sleep(1)
     end = time.time()
-    print('finished\ncost: {}', str(end-start))
+    print('finished\ncost: {}'.format(str(end-start)))
--- snake_insight_threading/core/Filter.py
+++ snake_insight_threading/core/Filter.py
@@ -1,22 +1,31 @@
+import math
+import re
 import time
 import numpy as np
 from snake_insight_threading.util import RedisCommand
 
 
 class Filter(object):
-    redis = RedisCommand(host='10.242.91.125')
+    # redis = RedisCommand(host='10.242.91.125')
+    redis = RedisCommand(db=1)
     city_infos = []
-    last_area = ''
+    last_region = ''
 
-    def __init__(self, area=None):
-        self.get_city_info_by_area(area)
+    def __init__(self, region=None):
+        self.get_city_info_by_area(region)
 
-    def get_city_info_by_area(self, area):
-        if area is None or area == self.last_area:
+    def __init_before_process(self, region=None):
+        if region is None:
+            region = self.last_region
+
+        self.get_city_info_by_area(region)
+
+    def get_city_info_by_area(self, region):
+        if region is None or region == self.last_region:
             return self.city_infos
 
         self.city_infos = []
-        house_info_keys = self.redis.smembers(area)
+        house_info_keys = self.redis.smembers(region)
 
         # 使用pineline将指令一起发送可以从70秒降低到3秒
         with self.redis.pipeline(transaction=False) as p:
@@ -25,17 +34,38 @@ class Filter(object):
 
             result = p.execute()
             for house_info in result:
+                # 转为utf-8编码
                 house_info_utf8 = {k.decode('utf8'): v.decode('utf8') for k, v in house_info.items()}
+                # 将tags的值转为列表
                 house_info_utf8['tags'] = str(house_info_utf8.get('tags')[1:-1]).split(',')
+                house_info_utf8['has_elevator'] = False
+
+                # 对tags进行遍历，去掉多余的单引号，判断是否有电梯
+                for i, tag in enumerate(house_info_utf8['tags']):
+                    sub_tag = re.search(r"('(.*)?')", tag).group(2)
+
+                    if sub_tag == '有电梯':
+                        house_info_utf8['has_elevator'] = True
+
+                    house_info_utf8['tags'][i] = sub_tag
+
                 self.city_infos.append(house_info_utf8)
 
-        self.last_area = area
+        self.last_area = region
         return self.city_infos
 
-    def get_room_price(self, area):
-        self.get_city_info_by_area(area)
+    def get_room_price(self, region=None):
+        """
+        厅室数量-价格
+        """
+        self.__init_before_process(region)
 
         # key为living_room和bedroom构成的元组，value为列表，分别为平均价 最高价 最低价 统计数量
+        # 例如： {
+        #        ('1', '3'): [4342.133555926544, 14800.0, 990.0, 599],
+        #        ('2', '2'): [5055.043143297381, 16500.0, 1900.0, 1298],
+        #        ...
+        #       }
         statisticians_result = {}
 
         for house_info in self.city_infos:
@@ -43,23 +73,125 @@ class Filter(object):
             bedroom = house_info.get('bedroom')
             price = float(house_info.get('price'))
 
-            statisticians = statisticians_result.get((living_room, bedroom),[0, price, price, 0])
+            statisticians = statisticians_result.get((living_room, bedroom), [0, price, price, 0])
             statisticians[0] += price
             statisticians[1] = max(statisticians[1], price)
             statisticians[2] = min(statisticians[2], price)
             statisticians[3] += 1
             statisticians_result[(living_room, bedroom)] = statisticians
 
+        # 计算平均价格
         for k in statisticians_result:
-            statisticians_result[k][0] = statisticians_result[k][0]/statisticians_result[k][3]
+            statisticians_result[k][0] = statisticians_result[k][0] / statisticians_result[k][3]
 
         return statisticians_result
 
+    def get_floor_price(self, region=None):
+        """
+        （有无电梯）楼层——每平方米价格（柱状图）
+        """
+        self.__init_before_process(region)
+
+        statisticians_result = {}
+
+        for house_info in self.city_infos:
+            floor = house_info.get('floor')
+            if floor is None or floor == '':
+                continue
+            has_elevator = house_info.get('has_elevator')
+            price = float(house_info.get('price'))
+            space = float(house_info.get('space'))
+            price_per_square = price / space
+
+            level_dic = {'低': 1 / 6, '中': 1 / 2, '高': 5 / 6}
+            level = level_dic.get(floor[0], 0.0)
+
+            total = re.search(r'共([0-9]+)层', floor)
+            if total is None:
+                continue
+            else:
+                total = float(total.group(1))
+            floor = math.ceil(total * level)
+            # key为floor(int)构成的元组，value为列表，分别为平均每平方米价格 最高每平方米价格 最低每平方米价格 统计数量
+            # 例如：{
+            #       (7,): [69.90145372704634, 275.0, 21.59709618874773, 244],
+            #       (38,): [78.95237587138996, 107.98122065727699, 60.60606060606061, 4],
+            #       ...
+            #      }
+            tuple_k = (floor, has_elevator)
+            statisticians = statisticians_result.get(tuple_k, [0, price_per_square, price_per_square, 0])
+            statisticians[0] += price_per_square
+            statisticians[1] = max(price_per_square, statisticians[1])
+            statisticians[2] = min(price_per_square, statisticians[2])
+            statisticians[3] += 1
+
+            statisticians_result[tuple_k] = statisticians
+
+        # 计算平均价格
+        for k in statisticians_result:
+            statisticians_result[k][0] = statisticians_result[k][0] / statisticians_result[k][3]
+
+        return statisticians_result
+
+    def get_space_price_per_square(self, region=None):
+        """
+        面积-每平方米价格
+        """
+        self.__init_before_process(region)
+
+        statisticians_result = {}
+
+        for house_info in self.city_infos:
+            space = float(house_info.get('space'))
+            price = float(house_info.get('price'))
+            price_per_square = price / space
+
+            tuple_k = ('{:.2f}'.format(space),)
+            # key为space(2位小数)构成的元组，value为列表，分别为平均每平方米价格 最高每平方米价格 最低每平方米价格 统计数量
+            # 例如：{
+            #       ('28.00',): [53.022921108742004, 100.0, 24.571428571428573, 134],
+            #       ('60.00',): [57.494411177644714, 108.33333333333333, 16.666666666666668, 167],
+            #       ...
+            #      }
+            statisticians = statisticians_result.get(tuple_k, [0, price_per_square, price_per_square, 0])
+            statisticians[0] += price_per_square
+            statisticians[1] = max(price_per_square, statisticians[1])
+            statisticians[2] = min(price_per_square, statisticians[2])
+            statisticians[3] += 1
+
+            statisticians_result[tuple_k] = statisticians
+
+        # 计算平均价格
+        for k in statisticians_result:
+            statisticians_result[k][0] = statisticians_result[k][0] / statisticians_result[k][3]
+
+        return statisticians_result
+
+    def get_area_price(self, region=None):
+        """
+        区域各地段平均价格
+        """
+        self.__init_before_process(region)
+        statisticians_result = {}
+
+        for house_info in self.city_infos:
+            area = house_info.get('area')
+            price = float(house_info.get('price'))
+            tuple_k = (area,)
+            statisticians = statisticians_result.get(tuple_k, [0, price, price, 0])
+            statisticians[0] += price
+            statisticians[1] = max(price, statisticians[1])
+            statisticians[2] = min(price, statisticians[2])
+            statisticians[3] += 1
+
+        # 计算平均价格
+        for k in statisticians_result:
+            statisticians_result[k][0] = statisticians_result[k][0] / statisticians_result[k][3]
 
 if __name__ == "__main__":
     filter = Filter()
     start = time.time()
-    res = filter.get_room_price('海珠')
+    res = filter.get_floor_price('海珠')
     end = time.time()
     print(res)
     print('finished\ncost: {}'.format(str(end - start)))
