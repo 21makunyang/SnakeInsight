57cf17ba37b961176f5c5401d66d333ce3dda664
mky <1748438065@qq.com>
2024-06-24 19:43:01 +0800

    refactor(server): optimize elevator feature representation
    Change the boolean flag for has_elevator to an integer (1/0) to improve
    the consistency and clarity of feature representation in the prediction
    models. Also, update the __all__ list in core module to exclude Cleaner.

--- snake_insight_threading/core/__init__.py
+++ snake_insight_threading/core/__init__.py
@@ -2,7 +2,6 @@ from .Spider import Spider
 from .Parser import Parser
 from .Processor import Processor
 from .Controller import Controller
-from .Cleaner import Cleaner
 
 
-__all__ = ['Controller', 'Spider', 'Parser', 'Processor', 'Cleaner']
+__all__ = ['Controller', 'Spider', 'Parser', 'Processor']
